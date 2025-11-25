d91b0a1523ab248e250828cffcabdda7707e2f0a
中屋暢胡 <nakayatooruko@nakayatoorukonoMacBook-Air-2.local>
2025-11-25 21:00:33 +0900

    Add initial PostgreSQL migration

---
+++ prisma/migrations/20251125120019_init/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "date" TEXT NOT NULL,
+    "body" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
